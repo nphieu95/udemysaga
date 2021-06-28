09f530aa428a0eef81167dd0a3d7e19a450f902c
vuongbui <vuongbui@vnnginfotech.com>
2021-06-28 17:16:14 +0700

    first commit

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,28 @@
+import { UserActionTypes } from "./user.types";
+
+const INITIAL_STATE = {
+  currentUser: null,
+  error: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.GOOGLE_SIGN_IN_SUCCES:
+    case UserActionTypes.EMAIL_SIGN_IN_SUCCES:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null,
+      };
+    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
